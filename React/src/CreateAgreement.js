46dad6268e27dfe473243a746a4f3a34a6851dcc
Alec Hajinoff <hajinoff@gmail.com>
2025-09-30 06:18:36 +0100

    30.9.25, 6.18

--- React/src/CreateAgreement.js
+++ React/src/CreateAgreement.js
@@ -63,33 +63,33 @@ function CreateAgreement() {
   };
 
   const handleHashChange = async (e) => {
-      const hash = e.target.value;
-      setAgreementHash(hash);
-  
-      if (hash.length > 0) {
-        try {
-          const data = await agreementHashUserDashboard(hash); // Checks the hash as the user inserts and when that matches displays the agreement text.
-          if (data.status === "success") {
-            setAgreementText(data.agreementText);
-            setErrorMessage("");
-          } else {
-            setAgreementText("");
-            setErrorMessage(
-              "Incorrect hash, please ask the agreement owner for the correct hash"
-            );
-          }
-        } catch (error) {
-          setErrorMessage(error.message);
+    const hash = e.target.value;
+    setAgreementHash(hash);
+
+    if (hash.length > 0) {
+      try {
+        const data = await agreementHashUserDashboard(hash); // Checks the hash as the user inserts and when that matches displays the agreement text.
+        if (data.status === "success") {
+          setAgreementText(data.agreementText);
+          setErrorMessage("");
+        } else {
           setAgreementText("");
+          setErrorMessage(
+            "Incorrect hash, please ask the agreement owner for the correct hash"
+          );
         }
-      } else {
+      } catch (error) {
+        setErrorMessage(error.message);
         setAgreementText("");
-        setErrorMessage("");
       }
-    };
+    } else {
+      setAgreementText("");
+      setErrorMessage("");
+    }
+  };
 
   const filteredCountersignedAgreements = agreements
-      .filter(
+    .filter(
       (agreement) => agreement.counter_signed && agreement.needs_signature
     )
     .filter((agreement) => agreement.category === activeTab);
@@ -104,43 +104,47 @@ function CreateAgreement() {
     <div className="container text-center">
       <div>
         <p>
-          Whether your agreement needs a countersignature or not, the system securely logs it. Countersigned agreements are anchored on the blockchain, and agreements that donÔÇÖt need a signature appear in their own dashboard view.
+          Whether your agreement needs a countersignature or not, the system
+          securely logs it. Countersigned agreements are anchored on the
+          blockchain, and agreements that donÔÇÖt need a signature appear in
+          their own dashboard view.
         </p>
       </div>
       <div className="d-flex justify-content-end mb-3">
         <LogoutComponent />
       </div>
       <form onSubmit={handleSubmit}>
-              <div className="form-group mb-3">
-                <label htmlFor="agreementCategory">
-                  Step 1: Select the category your agreement relates to from the
-                  dropdown menu below:
-                </label>
-                <select
-                  required
-                  id="agreementCategory"
-                  className="form-control"
-                  name="category"
-                  value={formData.category || ""}
-                  onChange={handleChange}
-                >
-                  <option value="" disabled>
-                    Select a category
-                  </option>
-                  <option value="Clients">Clients</option>
-                  <option value="Suppliers">Suppliers</option>
-                  <option value="Operations">Operations</option>
-                  <option value="HR">HR</option>
-                  <option value="Marketing">Marketing</option>
-                  <option value="Finance">Finance</option>
-                  <option value="Other">Other</option>
-                </select>
-              </div>
+        <div className="form-group mb-3">
+          <label htmlFor="agreementCategory">
+            Step 1: Select the category your agreement relates to from the
+            dropdown menu below:
+          </label>
+          <select
+            required
+            id="agreementCategory"
+            className="form-control"
+            name="category"
+            value={formData.category || ""}
+            onChange={handleChange}
+          >
+            <option value="" disabled>
+              Select a category
+            </option>
+            <option value="Clients">Clients</option>
+            <option value="Suppliers">Suppliers</option>
+            <option value="Operations">Operations</option>
+            <option value="HR">HR</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Finance">Finance</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
 
         <div className="form-group mb-3">
           <label htmlFor="agreementText">
-            Step 2: Copy the agreement from your email or file, paste it into the text
-            box below, choose if your agreement needs a counter signature, then click 'Generate hash'.
+            Step 2: Copy the agreement from your email or file, paste it into
+            the text box below, choose if your agreement needs a counter
+            signature, then click 'Generate hash'.
           </label>
           <textarea
             id="agreementText"
@@ -153,76 +157,82 @@ function CreateAgreement() {
           />
         </div>
         <div className="form-group mb-3">
-                  <label>
-                    Some agreements need a countersignature - like those with clients or
-                    external partners. Others may not - like internal commitments
-                    between colleagues. Does your agreement need to be confirmed by the
-                    other party?
-                  </label>
-                  <div className="form-check form-check-inline">
-                    <input
-                      type="radio"
-                      className="form-check-input"
-                      id="needsSignatureYes"
-                      name="needs_signature"
-                      value={1}
-                      required
-                      checked={formData.needs_signature === 1}
-                      onChange={(e) => {
-                        setFormData({
-                          ...formData,
-                          needs_signature: parseInt(e.target.value),
-                        });
-                        setTextHash("");
-                      }}
-                    />
-                    <label className="form-check-label" htmlFor="needsSignatureYes">
-                      Yes
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      type="radio"
-                      className="form-check-input"
-                      id="needsSignatureNo"
-                      name="needs_signature"
-                      value={0}
-                      required
-                      checked={formData.needs_signature === 0}
-                      onChange={(e) => {
-                        setFormData({
-                          ...formData,
-                          needs_signature: parseInt(e.target.value),
-                        });
-                        setTextHash("");
-                      }}
-                    />
-                    <label className="form-check-label" htmlFor="needsSignatureNo">
-                      No
-                    </label>
-                  </div>
-                </div>
-        
-                {formData.needs_signature === 0 && (
-                  <div className="form-group mb-3">
-                    <label htmlFor="agreementTag">Agreement relates to (this is how you will find it in the dashboard):</label>
-                    <input
-                      type="text"
-                      id="agreementTag"
-                      className="form-control"
-                      name="agreement_tag"
-                      value={formData.agreement_tag || ""}
-                      onChange={handleChange}
-                      required={formData.needs_signature === 1}
-                      placeholder="e.g. Marketing agreement with Mike"
-                    />
-                  </div>
-                )}
+          <label>
+            Some agreements need a countersignature - like those with clients or
+            external partners. Others may not - like internal commitments
+            between colleagues. Does your agreement need to be confirmed by the
+            other party?
+          </label>
+          <div className="form-check form-check-inline">
+            <input
+              type="radio"
+              className="form-check-input"
+              id="needsSignatureYes"
+              name="needs_signature"
+              value={1}
+              required
+              checked={formData.needs_signature === 1}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  needs_signature: parseInt(e.target.value),
+                });
+                setTextHash("");
+              }}
+            />
+            <label className="form-check-label" htmlFor="needsSignatureYes">
+              Yes
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              type="radio"
+              className="form-check-input"
+              id="needsSignatureNo"
+              name="needs_signature"
+              value={0}
+              required
+              checked={formData.needs_signature === 0}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  needs_signature: parseInt(e.target.value),
+                });
+                setTextHash("");
+              }}
+            />
+            <label className="form-check-label" htmlFor="needsSignatureNo">
+              No
+            </label>
+          </div>
+        </div>
+
+        {formData.needs_signature === 0 && (
+          <div className="form-group mb-3">
+            <label htmlFor="agreementTag">
+              Agreement relates to (this is how you will find it in the
+              dashboard):
+            </label>
+            <input
+              type="text"
+              id="agreementTag"
+              className="form-control"
+              name="agreement_tag"
+              value={formData.agreement_tag || ""}
+              onChange={handleChange}
+              required={formData.needs_signature === 1}
+              placeholder="e.g. Marketing agreement with Mike"
+            />
+          </div>
+        )}
 
         {/* Display hash if available */}
         {textHash && (
           <div className="alert alert-info">
-            <strong>Agreement hash. To view this agreement in your dashboard, please refresh the page.</strong>
+            <strong>
+              Agreement hash. To view this agreement in your dashboard, please
+              refresh the page.
+            </strong>
             <br />
             <code>{textHash}</code>
           </div>
@@ -244,26 +254,26 @@ function CreateAgreement() {
         </div>
 
         {formData.needs_signature === 1 && (
-        <div className="form-group mb-3">
-          <label>
-            Step 3: Copy the agreement hash above and email it to the other
-            party together with this link:{" "}
-            <a
-              href="https://agreementlog.com/CounterSignature"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://agreementlog.com/CounterSignature
-            </a>{" "}
-            <br /> Ask them to open the link, enter the agreement hash, review
-            the agreement text, and click 'Countersign' if they agree. <br />{" "}
-            Once they have countersigned, the agreement will appear as
-            countersigned in the table below.
-          </label>
-        </div>
+          <div className="form-group mb-3">
+            <label>
+              Step 3: Copy the agreement hash above and email it to the other
+              party together with this link:{" "}
+              <a
+                href="https://agreementlog.com/CounterSignature"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://agreementlog.com/CounterSignature
+              </a>{" "}
+              <br /> Ask them to open the link, enter the agreement hash, review
+              the agreement text, and click 'Countersign' if they agree. <br />{" "}
+              Once they have countersigned, the agreement will appear as
+              countersigned in the table below.
+            </label>
+          </div>
         )}
         <div className="form-group mb-3">
-        <div className="mt-4">
+          <div className="mt-4">
             <label className="step-label">
               Agreements created but not yet countersigned
             </label>
@@ -289,30 +299,30 @@ function CreateAgreement() {
 
             <label className="table-label mt-4">Agreements countersigned</label>
             {/* Tabs for filtering countersigned agreements */}
-                        <div className="tabs-container mb-3">
-                          <div className="nav nav-tabs">
-                            {[
-                              "Clients",
-                              "Suppliers",
-                              "Operations",
-                              "HR",
-                              "Marketing",
-                              "Finance",
-                              "Other",
-                            ].map((category) => (
-                              <button
-                                key={category}
-                                className={`nav-link ${
-                                  activeTab === category ? "active" : ""
-                                }`}
-                                onClick={() => setActiveTab(category)}
-                                type="button"
-                              >
-                                {category}
-                              </button>
-                            ))}
-                          </div>
-                        </div>
+            <div className="tabs-container mb-3">
+              <div className="nav nav-tabs">
+                {[
+                  "Clients",
+                  "Suppliers",
+                  "Operations",
+                  "HR",
+                  "Marketing",
+                  "Finance",
+                  "Other",
+                ].map((category) => (
+                  <button
+                    key={category}
+                    className={`nav-link ${
+                      activeTab === category ? "active" : ""
+                    }`}
+                    onClick={() => setActiveTab(category)}
+                    type="button"
+                  >
+                    {category}
+                  </button>
+                ))}
+              </div>
+            </div>
             <table className="table">
               <thead>
                 <tr>
@@ -322,13 +332,13 @@ function CreateAgreement() {
                 </tr>
               </thead>
               <tbody>
-                 {filteredCountersignedAgreements.map((agreement) => (
-                    <tr key={agreement.agreement_hash}>
-                      <td>{agreement.countersigner_name}</td>
-                      <td>{agreement.countersigned_timestamp}</td>
-                      <td>{agreement.agreement_hash}</td>
-                    </tr>
-                  ))}
+                {filteredCountersignedAgreements.map((agreement) => (
+                  <tr key={agreement.agreement_hash}>
+                    <td>{agreement.countersigner_name}</td>
+                    <td>{agreement.countersigned_timestamp}</td>
+                    <td>{agreement.agreement_hash}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
             <label className="table-label mt-4">
